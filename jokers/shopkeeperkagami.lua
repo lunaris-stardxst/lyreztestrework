09c060edd78185e24572b9276ea460bca11898d4
Jairus Azarael Bona <webmaster@milkyway.moe>
2025-08-18 21:59:18 +0800

    finally got skk joker working :3

---
+++ jokers/shopkeeperkagami.lua
@@ -0,0 +1,72 @@
+SMODS.Joker{ --Shop Keeper Kagami
+    key = "shopkeeperkagami",
+    config = {
+        extra = {
+            discount_amount = 50
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Shop Keeper Kagami',
+        ['text'] = {
+            [1] = 'Gives a 50% discount',
+            [2] = 'on every shop item.'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 3,
+        y = 2
+    },
+    cost = 4,
+    rarity = 1,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers',
+    in_pool = function(self, args)
+          return (
+          not args
+
+          or args.source == 'sho' or args.source == 'buf' or args.source == 'jud' or args.source == 'rif' or args.source == 'rta' or args.source == 'sou' or args.source == 'uta' or args.source == 'wra'
+          )
+          and true
+      end
+    ,
+
+    calculate = function(self, card, context)
+    end,
+
+    add_to_deck = function(self, card, from_debuff)
+        G.E_MANAGER:add_event(Event({
+    func = function()
+        for k, v in pairs(G.I.CARD) do
+            if v.set_cost then v:set_cost() end
+        end
+        return true
+    end
+}))
+    end,
+
+    remove_from_deck = function(self, card, from_debuff)
+        G.E_MANAGER:add_event(Event({
+    func = function()
+        for k, v in pairs(G.I.CARD) do
+            if v.set_cost then v:set_cost() end
+        end
+        return true
+    end
+}))
+    end
+}
+
+local card_set_cost_ref = Card.set_cost
+function Card:set_cost()
+    card_set_cost_ref(self)
+
+    if next(SMODS.find_card("j_mktjk_shopkeeperkagami")) then
+        self.cost = math.max(0, math.floor(self.cost * (1 - 50 / 100))) end
+    end
