8b60a81abe7429dafdfd55efa9d6c9632285a023
MetaNite64 <noahtrivedi@gmail.com>
2025-09-02 21:50:41 -0500

    (ROT FOR CLOUT) gain x0.2 mult, always rental

--- content/jokers/rotforclout.lua
+++ content/jokers/rotforclout.lua
@@ -3,6 +3,7 @@ SMODS.Joker{ --ROT FOR CLOUT
     config = {
         extra = {
             multforclout = 1,
+            decrementforclout = 0.2,
             sell_value = 2
         }
     },
@@ -11,7 +12,7 @@ SMODS.Joker{ --ROT FOR CLOUT
         ['text'] = { -- fixed formatting and colors
             [1] = 'Sell value goes down',
             [2] = 'every round but adds',
-            [3] = '{X:mult,C:white}X0.5{} Mult for {C:attention}every round{}',
+            [3] = '{X:mult,C:white}X#2#{} Mult for {C:attention}every round{}',
             [4] = 'this card is held in hand.',
             [5] = '{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult){}'
         },
@@ -33,12 +34,15 @@ SMODS.Joker{ --ROT FOR CLOUT
     atlas = 'jokers',
 
     loc_vars = function(self, info_queue, card)
-        return {vars = {card.ability.extra.multforclout}}
+        return {vars = {card.ability.extra.multforclout, card.ability.extra.decrementforclout}}
     end,
     add_to_deck = function (self, card, from_debuff) -- this does something when the joker is obtained
         if not from_debuff then
             card.ability.extra_value = 6 -- needed so that the sell value is correct when obtained
         end
+    end,
+    set_ability = function(self, card, initial, delay_sprites)
+        card:set_rental(true)
     end,
     calculate = function(self, card, context)
         if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then -- added blueprint check to make sure that the cost change isnt triggered by copy effects
@@ -46,7 +50,7 @@ SMODS.Joker{ --ROT FOR CLOUT
                 -- changed 0 to -6 because thats the minimum that card.ability.extra_value should be
                 card.ability.extra_value = math.max(-6, (card.ability.extra_value or 0) - card.ability.extra.sell_value) -- moved outside func
                 card:set_cost()
-                card.ability.extra.multforclout = card.ability.extra.multforclout + 0.5
+                card.ability.extra.multforclout = card.ability.extra.multforclout + card.ability.extra.decrementforclout
                 return {
                     message = "-"..tostring(card.ability.extra.sell_value).." Sell Value",
                     colour = G.C.GREEN, -- moved outside extra
