60c0954dabe1d9efbca140a9795a924dceca04c1
lyrelryre <willow.the.sxlly@gmail.com>
2025-09-03 21:24:39 -0400

    added the rarity

--- content/rarity.lua
+++ content/rarity.lua
@@ -1,1 +1,11 @@
-
+SMODS.Rarity {
+    key = "useless",
+    default_weight = 0.5,
+    badge_colour = HEX("7f7fb1"),
+    loc_txt = {
+        name = "Useless"
+    },
+    get_weight = function(self, weight, object_type)
+        return weight
+    end,
+}
