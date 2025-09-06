cc2febdf57a4c80e4b95824e6f119def4b53d6a7
Jairus Azarael Bona <webmaster@milkyway.moe>
2025-09-06 23:22:36 +0800

    beta 6 (WE'RE NOT DONE UNTIL I SAY WE'RE DONE)

---
+++ content/jokers/iflashquad.lua
@@ -0,0 +1,39 @@
+SMODS.Joker{ --iFlash Quad
+    key = "iflashquad",
+    config = {
+        extra = {
+        }
+    },
+    loc_txt = {
+        ['name'] = 'iFlash Quad',
+        ['text'] = {
+            [1] = '{C:attention}+4{} hand size.'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 9,
+        y = 4
+    },
+    cost = 8,
+    rarity = 3,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'jokers',
+
+    calculate = function(self, card, context)
+    end,
+
+    add_to_deck = function(self, card, from_debuff)
+        G.hand:change_size(4)
+    end,
+
+    remove_from_deck = function(self, card, from_debuff)
+        G.hand:change_size(-4)
+    end
+}
---
+++ content/jokers/melodynosurname.lua
@@ -0,0 +1,55 @@
+SMODS.Joker{ --Melody Nosurname
+    key = "melodynosurname",
+    config = {
+        extra = {
+            xchips = 2,
+            handsize = 5
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Melody Nosurname',
+        ['text'] = {
+            [1] = 'Gives {X:blue,C:white}#1#X{} Chips when',
+            [2] = 'no {C:attention}face cards{} are played',
+            [3] = 'in a {C:attention}#2# card hand{}.'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.extra.xchips, card.ability.extra.handsize}}
+    end,
+    pos = {
+        x = 6,
+        y = 4
+    },
+    cost = 8,
+    rarity = 2,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'jokers',
+
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_main  then
+            if ((function()
+    local rankFound = true
+    for i, c in ipairs(context.full_hand) do
+        if c:is_face() then
+            rankFound = false
+            break
+        end
+    end
+    
+    return rankFound
+end)() and #context.full_hand == card.ability.extra.handsize) then
+                return {
+                    x_chips = card.ability.extra.xchips
+                }
+            end
+        end
+    end
+}
