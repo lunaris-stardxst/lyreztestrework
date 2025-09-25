a0570215788c1dd156b6028382c46ddc2fd3264b
lyrelryre <willow.the.sxlly@gmail.com>
2025-09-25 16:49:32 -0400

    jimmy

--- content/rarity.lua
+++ content/rarity.lua
@@ -1,4 +1,4 @@
-SMODS.Rarity {
+SMODS.Rarity { --WAIT THRES ALREADY A RARITY LUA FUUCCKK
     key = "jimmy",
     default_weight = 0.25,
     badge_colour = HEX("7f7fb1"),
@@ -6,28 +6,28 @@ SMODS.Rarity {
         name = "JIMMY"
     },
     get_weight = function(self, weight, object_type)
-        return weight --why the fuck is this jimmy
+        return weight --jmimy
     end,
 }
 SMODS.Rarity {
     key = "pseudolegend",
     default_weight = 0,
-    badge_colour = HEX("000000"),
+    badge_colour = HEX("a954b6"),
     loc_txt = {
         name = "Pseudo-Legendary"
     },
     get_weight = function(self, weight, object_type)
-        return weight --needs colour
+        return weight
     end,
 }
 SMODS.Rarity {
     key = "legendplus",
     default_weight = 0,
-    badge_colour = HEX("000000"),
+    badge_colour = HEX("b500ff"),
     loc_txt = {
         name = "Legendary+"
     },
     get_weight = function(self, weight, object_type)
-        return weight --needs colour
+        return weight
     end,
 }
