7edba42414d5ffe3aa0fb2115136cb03925c13c6
lyrelryre <willow.the.sxlly@gmail.com>
2025-09-25 16:53:09 -0400

    i searched for this... all for nothing...
    changed true kobayashi back to epic

--- content/jokers/truekobayashi.lua
+++ content/jokers/truekobayashi.lua
@@ -21,7 +21,7 @@ SMODS.Joker{ --True Kobayashi
     },
     pronouns = "she_her",
     cost = 20,
-    rarity = 'mktjk_pseudolegend',
+    rarity = 'mktjk_epic',
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
